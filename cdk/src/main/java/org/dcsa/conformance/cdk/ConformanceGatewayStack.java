265c2b3d8827dff49aadc00004135cea0e132459
gj0dcsa <135594855+gj0dcsa@users.noreply.github.com>
2023-09-11 21:57:03 +0200

    AWS CDK, API Gateway, empty lambda

---
+++ cdk/src/main/java/org/dcsa/conformance/cdk/ConformanceGatewayStack.java
@@ -0,0 +1,89 @@
+package org.dcsa.conformance.cdk;
+
+import java.util.Arrays;
+import java.util.List;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.BundlingOptions;
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.CfnOutputProps;
+import software.constructs.Construct;
+import software.amazon.awscdk.DockerVolume;
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.apigatewayv2.alpha.AddRoutesOptions;
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpApi;
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpApiProps;
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpMethod;
+import software.amazon.awscdk.services.apigatewayv2.alpha.PayloadFormatVersion;
+import software.amazon.awscdk.services.apigatewayv2.integrations.alpha.HttpLambdaIntegration;
+import software.amazon.awscdk.services.apigatewayv2.integrations.alpha.HttpLambdaIntegrationProps;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.logs.RetentionDays;
+import software.amazon.awscdk.services.s3.assets.AssetOptions;
+
+import static java.util.Collections.singletonList;
+import static software.amazon.awscdk.BundlingOutput.ARCHIVED;
+
+public class ConformanceGatewayStack extends Stack {
+    public ConformanceGatewayStack(final App parent, final String id) {
+        this(parent, id, null);
+    }
+
+    public ConformanceGatewayStack(final Construct parent, final String id, final StackProps props) {
+        super(parent, id, props);
+
+        List<String> packagingInstructions = Arrays.asList(
+                "/bin/sh",
+                "-c",
+                "mvn clean install && cp /asset-input/target/conformance-lambda.jar /asset-output/"
+        );
+
+        BundlingOptions.Builder builderOptions = BundlingOptions.builder()
+                .command(packagingInstructions)
+                .image(Runtime.JAVA_17.getBundlingImage())
+                .volumes(singletonList(
+                        // Mount local .m2 repo to avoid download all the dependencies again inside the container
+                        DockerVolume.builder()
+                                .hostPath(System.getProperty("user.home") + "/.m2/")
+                                .containerPath("/root/.m2/")
+                                .build()
+                ))
+                .user("root")
+                .outputType(ARCHIVED);
+
+        Function conformanceLambda = new Function(this, "ConformanceGatewayLambda", FunctionProps.builder()
+                .runtime(Runtime.JAVA_17)
+                .code(Code.fromAsset("../lambda/", AssetOptions.builder()
+                        .bundling(builderOptions
+                                .command(packagingInstructions)
+                                .build())
+                        .build()))
+                .handler("org.dcsa.conformance.gateway.DcsaConformanceGatewayLambda")
+                .memorySize(1024)
+                .timeout(Duration.minutes(15))
+                .logRetention(RetentionDays.SEVEN_YEARS)
+                .build());
+
+        HttpApi httpApi = new HttpApi(this, "conformance-gateway-api", HttpApiProps.builder()
+                .apiName("conformance-gateway-api")
+                .build());
+
+        httpApi.addRoutes(AddRoutesOptions.builder()
+                .path("/conformance")
+                .methods(singletonList(HttpMethod.GET))
+                .integration(new HttpLambdaIntegration("ConformanceGatewayIntegration", conformanceLambda, HttpLambdaIntegrationProps.builder()
+                        .payloadFormatVersion(PayloadFormatVersion.VERSION_2_0)
+                        .build()))
+                .build());
+
+        new CfnOutput(this, "HttApi", CfnOutputProps.builder()
+                .description("Url for Http Api")
+                .value(httpApi.getApiEndpoint())
+                .build());
+    }
+}
---
+++ lambda/src/main/java/org/dcsa/conformance/gateway/DcsaConformanceGatewayLambda.java
@@ -0,0 +1,24 @@
+package org.dcsa.conformance.gateway;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+
+public class DcsaConformanceGatewayLambda
+    implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+  public APIGatewayProxyResponseEvent handleRequest(
+      final APIGatewayProxyRequestEvent input, final Context context) {
+    System.out.println("input = " + input + ", context = " + context);
+
+    return new APIGatewayProxyResponseEvent()
+        .withHeaders(Map.of("Content-Type", "application/json"))
+        .withStatusCode(200)
+        .withBody(new ObjectMapper().createObjectNode().put("message", "OK").toPrettyString());
+  }
+
+  public static void main(String[] args) {} // unused
+}

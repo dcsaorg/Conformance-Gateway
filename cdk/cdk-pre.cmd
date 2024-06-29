cd94dc682d8402ba92c3d11637280de804b0fdc8
gj0dcsa <135594855+gj0dcsa@users.noreply.github.com>
2024-06-29 10:50:13 +0200

    No more mvn install and redundant build on cdk deploy

--- cdk/cdk-pre.cmd
+++ cdk/cdk-pre.cmd
@@ -1,37 +1,7 @@
-cd ..\core
-call mvn clean install
+cd ..
+call mvn clean package
 
-cd ..\booking
-call mvn clean install
-
-cd ..\ebl
-call mvn clean install
-
-cd ..\ebl-issuance
-call mvn clean install
-
-cd ..\ebl-surrender
-call mvn clean install
-
-cd ..\jit
-call mvn clean install
-
-cd ..\ovs
-call mvn clean install
-
-cd ..\pint
-call mvn clean install
-
-cd ..\tnt
-call mvn clean install
-
-cd ..\sandbox
-call mvn clean install
-
-cd ..\lambda
-call mvn clean install
-
-cd ..\webui
+cd webui
 call npm install
 call ng build --configuration=%1%
 

7d27ce6c054c34377045946820ad7df768337d89
J. Koster <jacobjob.koster@dcsa.org>
2024-07-24 11:04:46 +0200

    STNG-125 - Add all scenarios test suite and add it to the GitHub pipeline (#108)

---
+++ spring-boot/src/test/java/org/dcsa/conformance/springboot/ConformanceApplicationTest.java
@@ -0,0 +1,92 @@
+package org.dcsa.conformance.springboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class ConformanceApplicationTest {
+
+  @Autowired
+  private ConformanceApplication app;
+
+  @LocalServerPort
+  private int port;
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @ParameterizedTest
+  @CsvSource({
+    "booking-200-conformance-auto-all-in-one",
+    "booking-200-reference-implementation-auto-all-in-one",
+    "ebl-300-conformance-si-only-auto-all-in-one",
+    "ebl-300-conformance-td-only-auto-all-in-one",
+//    "ebl-300-reference-implementation-auto-all-in-one", // Perhaps needs fix: STNG-128
+    "eblissuance-200-conformance-auto-all-in-one",
+    "eblissuance-300-conformance-auto-all-in-one",
+    "eblsurrender-200-conformance-auto-all-in-one",
+    "eblsurrender-300-conformance-auto-all-in-one",
+    "jit-120-conformance-auto-all-in-one",
+    "ovs-300-conformance-auto-all-in-one",
+    "pint-300-reference-implementation-auto-all-in-one",
+    "tnt-220-conformance-auto-all-in-one"
+  })
+  void testEachSuite(final String sandboxId) throws InterruptedException {
+    log.info("Starting scenario suite: {}", sandboxId);
+    // validate if scenario is listed
+    String rootURL = restTemplate.getForObject("http://localhost:" + port + "/", String.class);
+    Assumptions.assumeTrue(rootURL.contains(sandboxId), sandboxId + " not found in root URL, skipping in this branch!");
+
+    // Start the scenario
+    String value = restTemplate.getForObject("http://localhost:" + port + getAppURL(sandboxId, "reset"), String.class);
+    assertEquals("{}", value);
+
+    // Wait for the scenario to finish
+    checkUntilScenariosAreReady(sandboxId);
+
+    // Get the report and validate it. Should have 2 times "✅ CONFORMANT" in the report.
+    String report = restTemplate.getForObject("http://localhost:" + port + getAppURL(sandboxId, "report"), String.class);
+    int firstFound = report.indexOf("conformance</h2><details open><summary>✅ CONFORMANT");
+    int secondFound = report.indexOf("conformance</h2><details open><summary>✅ CONFORMANT", firstFound + 50);
+    if (firstFound == -1 || secondFound == -1) { // Report current situation for debugging
+      log.error("Report current situation: {}", report);
+    }
+    assertTrue(firstFound > 0, "First conformance OK not found");
+    assertTrue(secondFound > firstFound, "Second conformance OK not found");
+  }
+
+  private void checkUntilScenariosAreReady(String sandboxId) throws InterruptedException {
+    String status;
+    String previousStatus = "";
+    String startStatus = restTemplate.getForObject("http://localhost:" + port + getAppURL(sandboxId, "status"), String.class);
+    do {
+      Thread.sleep(3_000L);
+      status = restTemplate.getForObject("http://localhost:" + port + getAppURL(sandboxId, "status"), String.class);
+      if (status.equals(previousStatus)) { // Detection of a stuck scenario, prevent waiting forever. Note: turn off while debugging!
+        log.error("Status did not change: {}. Originally started at: {}", status, startStatus);
+        break;
+      }
+      log.info("Current status: {}", status);
+      previousStatus = status;
+      if (status.length() > "{\"scenariosLeft\":0}".length()) { // More than 9 scenarios left, wait longer
+        Thread.sleep(7_000L);
+      }
+    } while (!status.equals("{\"scenariosLeft\":0}"));
+    assertEquals("{\"scenariosLeft\":0}", status, "Scenario did not finish. Original start status: " + startStatus);
+    log.info("Original start status of sandboxId: {} was: {}", sandboxId, startStatus);
+  }
+
+  private String getAppURL(String scenarioID, String urlPath) {
+    return "/conformance/" + app.localhostAuthUrlToken + "/sandbox/" + scenarioID + "/" + urlPath;
+  }
+}
---
+++ spring-boot/src/test/java/org/dcsa/conformance/springboot/ConformanceBasicAPITest.java
@@ -0,0 +1,117 @@
+package org.dcsa.conformance.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Basic validation on API endpoints: testing content type, status code, and basic content.
+ * Does not run the actual conformance tests, because Tomcat is not started, so no actual endpoints are reachable by a httpClient.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class ConformanceBasicAPITest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ConformanceApplication app;
+
+  @Test
+  void shouldReturnDefaultHomepage() throws Exception {
+    mockMvc.perform(get("/"))
+//      .andDo(print())
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+      .andExpect(content().string(containsString("DCSA Conformance")))
+      .andExpect(content().string(containsString("booking-200-conformance-auto-all-in-one")))
+      .andExpect(content().string(containsString("tnt-220-conformance-auto-all-in-one")));
+  }
+
+  @Test
+  void shouldStartBookingScenario() throws Exception {
+    String sandboxId = "booking-200-conformance-auto-all-in-one";
+    mockMvc.perform(get(getAppURL(sandboxId, "reset")))
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(content().string(containsString("{}")));
+  }
+
+  @Test
+  void shouldReturnStatus() throws Exception {
+    String sandboxId = "booking-200-conformance-auto-all-in-one";
+    mockMvc.perform(get(getAppURL(sandboxId, "reset"))).andExpect(status().isOk());
+
+    mockMvc.perform(get(getAppURL(sandboxId, "status")))
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(content().string(containsString("{\"scenariosLeft\":14}")));
+  }
+
+  @Test
+  void shouldReturnReport() throws Exception {
+    String sandboxId = "booking-200-conformance-auto-all-in-one";
+    mockMvc.perform(get(getAppURL(sandboxId, "reset"))).andExpect(status().isOk());
+
+    mockMvc.perform(get(getAppURL(sandboxId, "report")))
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+      .andExpect(content().string(containsString("Conformance Report")))
+      .andExpect(content().string(containsString("Carrier conformance")))
+      .andExpect(content().string(containsString("Shipper conformance")));
+  }
+
+  @Test
+  void shouldReturnPrintableReport() throws Exception {
+    String sandboxId = "booking-200-conformance-auto-all-in-one";
+    mockMvc.perform(get(getAppURL(sandboxId, "reset"))).andExpect(status().isOk());
+
+    mockMvc.perform(get(getAppURL(sandboxId, "printableReport")))
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+      .andExpect(content().string(containsString("Conformance Report")))
+      .andExpect(content().string(containsString("Carrier conformance")))
+      .andExpect(content().string(containsString("Shipper conformance")));
+
+    mockMvc.perform(get(getAppURL(sandboxId, "party/Carrier1/prompt/json")))
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(content().string(containsString("SupplyCSP [REGULAR]")));
+  }
+
+  @Test
+  void shouldReturnCarrier1Prompt() throws Exception {
+    String sandboxId = "booking-200-conformance-auto-all-in-one";
+    mockMvc.perform(get(getAppURL(sandboxId, "reset"))).andExpect(status().isOk());
+
+    mockMvc.perform(get(getAppURL(sandboxId, "party/Carrier1/prompt/json")))
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(content().string(containsString("SupplyCSP [REGULAR]")));
+  }
+
+  @Test
+  void shouldReturnPlatform1Prompt() throws Exception {
+    String sandboxId = "booking-200-conformance-auto-all-in-one";
+    mockMvc.perform(get(getAppURL(sandboxId, "reset"))).andExpect(status().isOk());
+
+    mockMvc.perform(get(getAppURL(sandboxId, "party/Platform1/prompt/json")))
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(content().string(containsString("[ ]")));
+  }
+
+  private String getAppURL(String scenarioID, String urlPath) {
+    return "/conformance/" + app.localhostAuthUrlToken + "/sandbox/" + scenarioID + "/" + urlPath;
+  }
+
+}

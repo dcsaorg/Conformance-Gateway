e165dc78bff57be34317e2bed35eb847a3432b7d
SangeethaPalat <sangeetha.palat@dcsa.org>
2024-08-12 19:24:12 +0200

    CS-changes-working on checks

---
+++ commercial-schedules/src/main/java/org/dcsa/conformance/standards/cs/checks/CsDataSets.java
@@ -0,0 +1,8 @@
+package org.dcsa.conformance.standards.cs.checks;
+
+import org.dcsa.conformance.core.check.KeywordDataset;
+
+public class CsDataSets {
+  public static final KeywordDataset RECEIPT_TYPE_AT_ORIGIN = KeywordDataset.staticDataset("CY", "SD", "CFS");
+  public static final KeywordDataset DELIVERY_TYPE_AT_DESTINATION = KeywordDataset.staticDataset("CY", "SD", "CFS");
+}

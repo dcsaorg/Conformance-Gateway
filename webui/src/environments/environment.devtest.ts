8d5b6bcad24f7b4a51162977cf25ec184ddd474d
gj0dcsa <135594855+gj0dcsa@users.noreply.github.com>
2024-11-17 18:36:42 +0100

    SD-1610 Replaced the DevTest stack with the DT one

--- webui/src/environments/environment.devtest.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  name: "devtest",
-  production: true,
-  apiBaseUrl: 'https://test-webui.conformance-development-1.dcsa.org/',
-  useCognito: true,
-  cognitoUserPoolId: 'eu-north-1_eJ0a04IOq',
-  cognitoClientId: '7cef5pjbik5n5hcck15ccbmq3g',
-  siteTitle: 'DCSA Conformance DevTest',
-};
---
+++ webui/src/environments/environment.dt.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  name: "dt",
+  production: true,
+  apiBaseUrl: 'https://dt-webui.conformance-dt-1.dcsa.org/',
+  useCognito: true,
+  cognitoUserPoolId: 'eu-north-1_It7UzW00Z',
+  cognitoClientId: '7r5r2a5v06otg47vm1vddhkvle',
+  siteTitle: 'DCSA Conformance DT',
+};

2f34cb7a6ee8e21645d8cd05f094ac0a49816682
J. Koster <j.koster@gmx.com>
2025-02-05 14:45:42 +0100

    Revert "SD-1628 Migrate to AWS SDK 2.0 (#257)"
    This reverts commit 6cd27e84d1434141ba99521220ff0885b6785bbd.

--- lambda/src/main/java/org/dcsa/conformance/lambda/LambdaToolkit.java
+++ lambda/src/main/java/org/dcsa/conformance/lambda/LambdaToolkit.java
@@ -1,5 +1,8 @@
 package org.dcsa.conformance.lambda;
 
+import com.amazonaws.services.lambda.AWSLambdaAsyncClientBuilder;
+import com.amazonaws.services.lambda.model.InvocationType;
+import com.amazonaws.services.lambda.model.InvokeRequest;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.util.function.Consumer;
 import lombok.AccessLevel;
@@ -7,21 +10,14 @@ import lombok.NoArgsConstructor;
 import org.dcsa.conformance.sandbox.state.ConformancePersistenceProvider;
 import org.dcsa.conformance.sandbox.state.DynamoDbSortedPartitionsLockingMap;
 import org.dcsa.conformance.sandbox.state.DynamoDbSortedPartitionsNonLockingMap;
-import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.lambda.LambdaAsyncClient;
-import software.amazon.awssdk.services.lambda.model.InvocationType;
-import software.amazon.awssdk.services.lambda.model.InvokeRequest;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class LambdaToolkit {
   public static ConformancePersistenceProvider createPersistenceProvider() {
     DynamoDbClient dynamoDbClient = DynamoDbClient.builder().region(Region.EU_NORTH_1).build();
     String tableName = System.getenv("TABLE_NAME");
-    if (tableName == null || tableName.isEmpty()) {
-      throw new IllegalStateException("Environment variable TABLE_NAME is not set");
-    }
     return new ConformancePersistenceProvider(
         new DynamoDbSortedPartitionsNonLockingMap(dynamoDbClient, tableName),
         new DynamoDbSortedPartitionsLockingMap(dynamoDbClient, tableName));
@@ -34,16 +30,12 @@ public class LambdaToolkit {
 
   public static Consumer<JsonNode> createDeferredSandboxTaskConsumer(
       ConformancePersistenceProvider persistenceProvider) {
-    return jsonNode -> {
-      try (LambdaAsyncClient client = LambdaAsyncClient.create()) {
-        InvokeRequest invokeRequest =
-            InvokeRequest.builder()
-                .invocationType(InvocationType.EVENT)
-                .functionName(getDbConfigValue(persistenceProvider, "sandboxTaskLambdaArn"))
-                .payload(SdkBytes.fromUtf8String(jsonNode.toPrettyString()))
-                .build();
-        client.invoke(invokeRequest);
-      }
-    };
+    return jsonNode ->
+        AWSLambdaAsyncClientBuilder.defaultClient()
+            .invoke(
+                new InvokeRequest()
+                    .withInvocationType(InvocationType.Event)
+                    .withFunctionName(getDbConfigValue(persistenceProvider, "sandboxTaskLambdaArn"))
+                    .withPayload(jsonNode.toPrettyString()));
   }
 }

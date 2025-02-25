4130cc581ab07a08e5c630b9248dd0f7d9e3c1c7
SangeethaPalat <sangeetha.palat@dcsa.org>
2025-02-25 14:39:07 +0100

    SD-503-ebl added oob prompt

--- ebl/src/main/java/org/dcsa/conformance/standards/ebl/action/UCX_Carrier_TDOnlyProcessOutOfBandUpdateOrAmendmentRequestDraftTransportDocumentAction.java
+++ ebl/src/main/java/org/dcsa/conformance/standards/ebl/action/UCX_Carrier_TDOnlyProcessOutOfBandUpdateOrAmendmentRequestDraftTransportDocumentAction.java
@@ -1,6 +1,7 @@
 package org.dcsa.conformance.standards.ebl.action;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.Map;
 import lombok.Getter;
 
 @Getter
@@ -15,9 +16,10 @@ public class UCX_Carrier_TDOnlyProcessOutOfBandUpdateOrAmendmentRequestDraftTran
 
   @Override
   public String getHumanReadablePrompt() {
-    // no markdown instructions needed: never expected to be performed by a human operator
-    return ("Process and accept an out of band request for a change to the TD with reference %s"
-        .formatted(getDspSupplier().get().transportDocumentReference()));
+    return getMarkdownHumanReadablePrompt(
+        Map.of("REFERENCE", getDSP().transportDocumentReference()), "prompt-carrier-oob.md");
+    /*    return ("Process and accept an out of band request for a change to the TD with reference %s"
+    .formatted(getDspSupplier().get().transportDocumentReference()));*/
   }
 
   @Override
--- ebl/src/main/resources/standards/ebl/instructions/prompt-carrier-uc6-si-td.md
+++ ebl/src/main/resources/standards/ebl/instructions/prompt-carrier-uc6-si-td.md
@@ -1,2 +1,3 @@
 Perform Use Case 6: Publish draft transport document for shipping instructions with reference REFERENCE
-*Set the transport document status to `DRAFT`.
+
+* Set the transport document status to `DRAFT`.

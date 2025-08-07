dba279f91d794cd964da54d18340d892c97a7be1
gj0dcsa <135594855+gj0dcsa@users.noreply.github.com>
2025-08-07 11:10:02 +0200

    SD-2363 AN locationTypeCode

--- specifications/src/main/java/org/dcsa/conformance/specifications/standards/an/v100/model/Location.java
+++ specifications/src/main/java/org/dcsa/conformance/specifications/standards/an/v100/model/Location.java
@@ -12,7 +12,7 @@ public class Location {
   private String locationName;
 
   @Schema()
-  private LocationTypeCode typeCode;
+  private LocationTypeCode locationTypeCode;
 
   @Schema(description = "Location address")
   private Address address;

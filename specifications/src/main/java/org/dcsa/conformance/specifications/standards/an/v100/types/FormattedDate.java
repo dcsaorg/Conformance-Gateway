e14c2a0f0ba24209f369e9e48d848484ec013918
gj0dcsa <135594855+gj0dcsa@users.noreply.github.com>
2025-08-13 17:27:11 +0200

    SD-2394 AN all dates are local dates

--- specifications/src/main/java/org/dcsa/conformance/specifications/standards/an/v100/types/FormattedDate.java
+++ specifications/src/main/java/org/dcsa/conformance/specifications/standards/an/v100/types/FormattedDate.java
@@ -8,5 +8,9 @@ import lombok.Data;
     type = "string",
     format = "date",
     example = "2025-01-23",
-    description = "String representation of a date in yyyy-MM-dd format.")
+    description =
+"""
+String representation of a date in yyyy-MM-dd format.
+Unless otherwise specified, it is a local date in the time zone of the implicitly associated event location.
+""")
 public class FormattedDate {}

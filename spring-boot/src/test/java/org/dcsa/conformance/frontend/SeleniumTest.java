2c97d3a7357851900bc9f9a7e7ff769bd64a7566
SangeethaPalat <sangeetha.palat@dcsa.org>
2025-08-21 12:56:52 +0200

    SD-2335-added AN to selenium test

--- spring-boot/src/test/java/org/dcsa/conformance/frontend/SeleniumTest.java
+++ spring-boot/src/test/java/org/dcsa/conformance/frontend/SeleniumTest.java
@@ -36,18 +36,20 @@ class SeleniumTest extends SeleniumTestBase {
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {
-    "Adoption", // Takes 0:29 minutes
-    "Booking", // Takes 11:52 minutes
-    "CS", // 11:05 minutes
-    "Ebl", // 37:09 minutes
-    "eBL Issuance", // 6:03 minutes
-    "eBL Surrender", // 7:59 minutes
-    "JIT", // 1:14 minutes
-    "OVS", // 3:34 minutes
-    "PINT", // 6:10 minutes
-    "TnT" // 6:20 minutes
-  })
+  @ValueSource(
+      strings = {
+        "Adoption", // Takes 0:29 minutes
+        "AN",
+        "Booking", // Takes 11:52 minutes
+        "CS", // 11:05 minutes
+        "Ebl", // 37:09 minutes
+        "eBL Issuance", // 6:03 minutes
+        "eBL Surrender", // 7:59 minutes
+        "JIT", // 1:14 minutes
+        "OVS", // 3:34 minutes
+        "PINT", // 6:10 minutes
+        "TnT" // 6:20 minutes
+      })
   void testStandardWithAllVersions(String standardName) {
     app.setSimulatedLambdaDelay(lambdaDelay);
     StopWatch stopWatch = StopWatch.createStarted();

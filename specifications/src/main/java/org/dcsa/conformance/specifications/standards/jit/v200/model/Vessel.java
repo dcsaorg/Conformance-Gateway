84e6207d6deaebe7a02805d80e4861c5b221ea38
gj0dcsa <135594855+gj0dcsa@users.noreply.github.com>
2025-09-23 07:50:30 +0200

    JIT 200 vessel status

--- specifications/src/main/java/org/dcsa/conformance/specifications/standards/jit/v200/model/Vessel.java
+++ specifications/src/main/java/org/dcsa/conformance/specifications/standards/jit/v200/model/Vessel.java
@@ -2,6 +2,7 @@ package org.dcsa.conformance.specifications.standards.jit.v200.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.dcsa.conformance.specifications.standards.core.v100.model.Location;
 import org.dcsa.conformance.specifications.standards.core.v100.types.VesselIMONumber;
 import org.dcsa.conformance.specifications.standards.core.v100.types.VesselMMSINumber;
 
@@ -15,41 +16,6 @@ public class Vessel {
 
   @Schema(maxLength = 50, example = "King of the Seas", description = "Vessel name")
   private String vesselName;
-
-  @Schema(
-      format = "float",
-      example = "245.45",
-      description =
-"""
-The maximum length of a ship's hull measured parallel to the waterline (Length OverAll).
-
-If the length is specified in feet (`FOT`) then the decimal part means a fraction of a foot and **not** as a number of inches.
-E.g. 120.5 feet means 120 and a half foot (which would be 120'6").
-""")
-  private Double lengthOverall;
-
-  @Schema(
-      format = "float",
-      example = "37.33",
-      description =
-"""
-Overall width of the ship measured at the widest point of the nominal waterline.
-
-If the width is specified in feet (`FOT`) then the decimal part means a fraction of a foot and **not** as a number of inches.
-E.g. 120.5 feet means 120 and a half foot (which would be 120'6").
-""")
-  private Double width;
-
-  @Schema(
-      maxLength = 3,
-      example = "MTR",
-      description =
-"""
-The unit of measure which can be expressed in:
-- `MTR` (Meter)
-- `FOT` (Foot)
-""")
-  private String dimensionUnit;
 
   @Schema(
       maxLength = 10,
@@ -81,4 +47,86 @@ Categorization of ocean-going vessels distinguished by the main cargo the vessel
 - `PILO` (Pilot boat)
 """)
   private String vesselTypeCode;
+
+  @Schema(description = "Current location of the vessel")
+  private Location vesselPosition;
+
+  @Schema(
+      example = "245.45",
+      description =
+"""
+Remaining distance reported by the vessel to the berth in the next destination port, expressed in nautical miles.
+""")
+  private Double milesToDestinationPort;
+
+  @Schema(
+      maxLength = 3,
+      example = "MTR",
+      description =
+"""
+The unit of measure in which the vessel size and draft are expressed:
+- `MTR` (Meter)
+- `FOT` (Foot)
+
+If a length is specified in feet (`FOT`) then the decimal part means a fraction of a foot and **not** as a number of inches.
+E.g. 120.5 feet means 120 and a half foot (which would be 120'6").
+""")
+  private String dimensionUnit;
+
+  @Schema(
+      format = "float",
+      example = "245.45",
+      description =
+"""
+The maximum length of a ship's hull measured parallel to the waterline (Length OverAll).
+""")
+  private Double lengthOverall;
+
+  @Schema(
+      format = "float",
+      example = "37.33",
+      description =
+"""
+Overall width of the ship measured at the widest point of the nominal waterline.
+""")
+  private Double width;
+
+  @Schema(
+      format = "float",
+      example = "55.0",
+      description =
+"""
+The vertical distance from the surface of the water to the highest point of mast or aerial.
+""")
+  private Double airDraft;
+
+  @Schema(
+      format = "float",
+      example = "12.5",
+      description =
+"""
+The vertical distance between the waterline and the bottom of the hull (keel) of a ship.
+The depth to which a ship is immersed in water.
+""")
+  private Double draft;
+
+  @Schema(
+      format = "float",
+      example = "37",
+      description =
+"""
+The vertical distance between the waterline and the bottom of the hull (keel) at the stern (after part) of the vessel.
+The depth to which the after part of the ship is immersed in water.
+""")
+  private Double aftDraft;
+
+  @Schema(
+      format = "float",
+      example = "35",
+      description =
+"""
+The vertical distance between the waterline and the bottom of the hull (keel) at the bow (forward part) of the vessel.
+The depth to which the forward part of the ship is immersed in water.
+""")
+  private Double forwardDraft;
 }

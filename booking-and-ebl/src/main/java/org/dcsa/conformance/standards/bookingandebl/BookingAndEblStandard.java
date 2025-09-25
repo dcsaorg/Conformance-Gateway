d58b6503b4f2ea9f436d56906bd161fa2811304b
Pedro Carvalho <pedro.carvalho@dcsa.org>
2025-09-25 12:32:05 +0200

    SD-355: Fixed sandbox configuration process

--- booking-and-ebl/src/main/java/org/dcsa/conformance/standards/bookingandebl/BookingAndEblStandard.java
+++ booking-and-ebl/src/main/java/org/dcsa/conformance/standards/bookingandebl/BookingAndEblStandard.java
@@ -7,6 +7,8 @@ import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.dcsa.conformance.core.AbstractComponentFactory;
 import org.dcsa.conformance.core.AbstractStandard;
 import org.dcsa.conformance.standards.booking.BookingStandard;
@@ -34,10 +36,45 @@ public class BookingAndEblStandard extends AbstractStandard {
   @Override
   public Map<String, Map<String, SortedMap<String, SortedSet<String>>>>
       getEndpointUrisAndMethodsByScenarioSuiteAndRoleName() {
-    Map<String, Map<String, SortedMap<String, SortedSet<String>>>> result = new HashMap<>();
+    Map<String, Map<String, SortedMap<String, SortedSet<String>>>> bookingResults =
+        BookingStandard.INSTANCE.getEndpointUrisAndMethodsByScenarioSuiteAndRoleName();
+    Map<String, Map<String, SortedMap<String, SortedSet<String>>>> eblResults =
+        EblStandard.INSTANCE.getEndpointUrisAndMethodsByScenarioSuiteAndRoleName();
 
-    result.putAll(BookingStandard.INSTANCE.getEndpointUrisAndMethodsByScenarioSuiteAndRoleName());
-    result.putAll(EblStandard.INSTANCE.getEndpointUrisAndMethodsByScenarioSuiteAndRoleName());
+    Map<String, SortedMap<String, SortedSet<String>>> mergedRoles = new HashMap<>();
+
+    for (var scenarioSuite : bookingResults.values()) {
+      for (var roleEntry : scenarioSuite.entrySet()) {
+        String roleName = roleEntry.getKey();
+        SortedMap<String, SortedSet<String>> endpoints = roleEntry.getValue();
+
+        mergedRoles.computeIfAbsent(roleName, k -> new TreeMap<>());
+        for (var endpointEntry : endpoints.entrySet()) {
+          String uri = endpointEntry.getKey();
+          SortedSet<String> methods = endpointEntry.getValue();
+
+          mergedRoles.get(roleName).computeIfAbsent(uri, k -> new TreeSet<>()).addAll(methods);
+        }
+      }
+    }
+
+    for (var scenarioSuite : eblResults.values()) {
+      for (var roleEntry : scenarioSuite.entrySet()) {
+        String roleName = roleEntry.getKey();
+        SortedMap<String, SortedSet<String>> endpoints = roleEntry.getValue();
+
+        mergedRoles.computeIfAbsent(roleName, k -> new TreeMap<>());
+        for (var endpointEntry : endpoints.entrySet()) {
+          String uri = endpointEntry.getKey();
+          SortedSet<String> methods = endpointEntry.getValue();
+
+          mergedRoles.get(roleName).computeIfAbsent(uri, k -> new TreeSet<>()).addAll(methods);
+        }
+      }
+    }
+
+    Map<String, Map<String, SortedMap<String, SortedSet<String>>>> result = new HashMap<>();
+    result.put(BookingAndEblScenarioListBuilder.SCENARIO_SUITE_CONFORMANCE, mergedRoles);
 
     return result;
   }

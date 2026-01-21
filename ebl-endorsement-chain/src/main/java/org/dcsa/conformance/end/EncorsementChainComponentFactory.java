183e0bf743f7554df5e7d7fbbb39621c7d86420b
SangeethaPalat <sangeetha.palat@dcsa.org>
2026-01-21 16:05:36 +0100

    SD-2697-ebl endorsementchain initial changes

---
+++ ebl-endorsement-chain/src/main/java/org/dcsa/conformance/end/EncorsementChainComponentFactory.java
@@ -0,0 +1,3 @@
+package org.dcsa.conformance.end;
+
+public class EncorsementChainComponentFactory {}
---
+++ ebl-endorsement-chain/src/main/java/org/dcsa/conformance/end/EndorsementChainStandard.java
@@ -0,0 +1,6 @@
+package org.dcsa.conformance.end;
+
+public class EndorsementChainStandard extends AbstractStandard{
+
+}
+

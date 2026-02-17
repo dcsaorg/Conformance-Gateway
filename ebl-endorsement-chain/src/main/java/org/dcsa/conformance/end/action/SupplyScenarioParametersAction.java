4c2f2afd75b0c07d52e510cc86237b679357c0e9
SangeethaPalat <sangeetha.palat@dcsa.org>
2026-02-17 11:58:35 +0100

    SD-2697-updated the values in sample response

--- ebl-endorsement-chain/src/main/java/org/dcsa/conformance/end/action/SupplyScenarioParametersAction.java
+++ ebl-endorsement-chain/src/main/java/org/dcsa/conformance/end/action/SupplyScenarioParametersAction.java
@@ -84,7 +84,8 @@ public class SupplyScenarioParametersAction extends EndorsementChainAction{
                         endorsementChainFilterParameter ->
                             switch (endorsementChainFilterParameter) {
                               case TRANSPORT_DOCUMENT_REFERENCE -> "HHL71800000";
-                              case TRANSPORT_DOCUMENT_SUB_REFERENCE -> "sub1234";
+                              case TRANSPORT_DOCUMENT_SUB_REFERENCE ->
+                                  "fc5009a7-25ad-4bb0-9892-4e2dea6bcdd9";
                               case CARRIER_SCAC_CODE -> "MAEU";
                               default -> "TODO";
                             })))

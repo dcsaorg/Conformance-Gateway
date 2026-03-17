269569542a51c79b9f957135b35f603f3583f303
SangeethaPalat <sangeetha.palat@dcsa.org>
2026-03-17 16:24:52 +0100

    Fix for deployment issue with Jest/test colliding with name test

---
+++ webui/src/environments/environment.testci.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  name: "test",
+  production: true,
+  apiBaseUrl: 'https://test-webui.conformance-test-1.dcsa.org/',
+  useCognito: true,
+  cognitoUserPoolId: 'eu-north-1_VRXXJfTsv',
+  cognitoClientId: '4b2c2dt168j0tkf1ubg16n3g42',
+  siteTitle: 'DCSA Conformance Test',
+};
